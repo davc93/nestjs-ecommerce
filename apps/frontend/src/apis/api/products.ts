27d3fd6924db438e6cad06eced252370308c5043
Diego Vergara Casanova <davc93@gmail.com>
2023-02-23 15:05:48 -0300

    fix: hardcoding data en frontend

--- apps/frontend/src/apis/api/products.ts
+++ apps/frontend/src/apis/api/products.ts
@@ -4,7 +4,7 @@ import { Product } from "../../models/product.model"
 
 export const getProducts = async () => {
     console.log(config)
-    const response =  await fetch(`${config.apiUrl}/products.json`)
+    const response =  await fetch(`${config.apiUrl}/products`)
     const products:Product[] = await response.json()
     return products
 }
